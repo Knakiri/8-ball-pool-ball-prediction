dac556582d221821443414ef2d81f8c624812bcf
Knakiri <95192545+Knakiri@users.noreply.github.com>
2022-08-16 22:45:13 +0200

    Update AdsManager.h

--- 8-Ball-Pool-Ball-Path-Prediction/src/AdsManager.h
+++ 8-Ball-Pool-Ball-Path-Prediction/src/AdsManager.h
@@ -8,7 +8,7 @@ struct AdsManager
 private:
 	struct Offsets
 	{
-		static constexpr SIZE_T SharedAdsManager        = 0x3526B0CUL;
+		static constexpr SIZE_T SharedAdsManager        = 0x35E0FCCUL;
 		static constexpr SIZE_T InterstitialsController = 0x84UL;
 	};
 };
