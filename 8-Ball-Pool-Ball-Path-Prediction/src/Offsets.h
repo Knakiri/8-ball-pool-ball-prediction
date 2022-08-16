fba87eb3e77aa86d9f5215fd73eb15d2d8d3a253
Knakiri <95192545+Knakiri@users.noreply.github.com>
2022-08-16 22:46:25 +0200

    Update Offsets.h

--- 8-Ball-Pool-Ball-Path-Prediction/src/Offsets.h
+++ 8-Ball-Pool-Ball-Path-Prediction/src/Offsets.h
@@ -6,6 +6,6 @@ namespace gOffsets
 {
 	constexpr SIZE_T SharedUserInfo           = 0x35AFD94UL;
 	constexpr SIZE_T SharedGameManager        = 0x34E7830UL;
-	constexpr SIZE_T CUE_PROPERTIES_MAX_POWER = 0x356DBC8UL;
-	constexpr SIZE_T CUE_PROPERTIES_SPIN      = 0x356DBD0UL;
+	constexpr SIZE_T CUE_PROPERTIES_MAX_POWER = 0x0356DBC8UL;
+	constexpr SIZE_T CUE_PROPERTIES_SPIN      = 0x0356DBD0UL;
 }
